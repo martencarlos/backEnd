babc8aaa210f93be925b01b69386576ee1099b73
Carlos Marten <martencarlos@gmail.com>
2018-12-21 11:34:44 +0100

    heroku debugging

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.set('views', [path.join(__dirname,'views'),
   path.join(__dirname,'products/users/register'),
   path.join(__dirname,'products/errors')
 ]);
-
+logger.info(path.join(__dirname,'views/layouts/partials/'));
 app.engine( 'hbs', hbs( { 
   extname: 'hbs', 
   defaultLayout: 'main', 
