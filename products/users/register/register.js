26a2a3548bfc123f4c28c84857e293c43dd22191
carlos marten <martencarlos@gmail.com>
2022-05-06 16:47:05 +0200

    Checking for duplicates in DB while registering a user now working :)

--- products/users/register/register.js
+++ products/users/register/register.js
@@ -14,45 +14,65 @@ var User = require('../user');
 router.get('/', function(req, res){
 	logger.info("rendering register");
 	res.render('register');
+
+	run()
+	async function run(){
+		const myuser =  await User.find({username: "test"});
+		console.log(myuser);
+	}
+	
 });
 
 // Register User
 router.post('/', function(req, res){
 	logger.info("entering post");
 	
-	var name = req.body.name;
-	var email = req.body.emails;
-	var username = req.body.username;
-	var password = req.body.password;
-	
-	
-	// Validation
-	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+	// async required because of database query
+	user = run()
+	async function run(){
+		//retrive info from Form
+		var name = req.body.name;
+		var email = req.body.emails;
+		var username = req.body.username;
+		var password = req.body.password;
+		
+		//Validation
+		// search if user exists in DB
+		const foundusername =  await User.find({username: username});
+		const foundemail =  await User.find({email: email});
+		
+		//validate user and password
+		if(foundusername.length!=0 || foundemail.length!=0 ){
+			req.checkBody('username', 'User is already registered').equals("not me");
+		}
+		req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
 
-	var errors = req.validationErrors();
+		var errors = req.validationErrors();
 
-	if(errors){
-		logger.info("showing errors");
-		res.render('register',{
-			errors:errors
-		});
-	} else {
-		var newUser = new User({
-			name: name,
-			email:email,
-			username: username,
-			password: password
-		});
+		if(errors){
+			logger.info("showing errors");
+			res.render('register',{
+				errors:errors
+			});
+		} else {
+			//create user in DB
+			var newUser = new User({
+				name: name,
+				email:email,
+				username: username,
+				password: password
+			});
 
-		User.createUser(newUser, function(err, user){
-			if(err) throw err;
+			User.createUser(newUser, function(err, user){
+				if(err) throw err;
+			});
 			
-		});
-
-		req.flash('success_msg', 'You are registered and can now login');
-
-		res.redirect('/users/login');
+			//redirect and show success message
+			req.flash('success_msg', 'You are registered and can now login');
+			res.redirect('/users/login');
+		}
 	}
+	
 });
 
 passport.use(new LocalStrategy(function(username, password, done) {
