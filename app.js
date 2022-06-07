d049a6dfaa96b79964cb479bc7ac3e6a2db1094e
Carlos Marten <martencarlos@gmail.com>
2022-06-07 13:39:33 +0200

    added local coverImage function

--- app.js
+++ app.js
@@ -304,6 +304,75 @@ app.post('/setImageProfile',checkAuthenticated, (req, res)=>{
 
 });
 
+app.post('/setCardCoverImage',checkAuthenticated, (req, res)=>{
+	console.log("setting Card Cover Image");
+  
+  //files are empty error handling
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+  
+  const file = req.files.cardCoverImage;
+  console.log(file)
+
+  const cookies = parseCookies(req)
+  var user = JSON.parse(cookies["user"].slice(2))
+  var url =""
+
+  const storage = getStorage(firebaseApp);
+
+  const metadata = {
+    contentType: file.mimetype
+  };
+
+  // Upload file and metadata to the object 'images/mountains.jpg'
+  const storageRef = ref(storage, user._id+'/cardImages/'+Date.now()+'-'+file.name);
+  const uploadTask = uploadBytesResumable(storageRef, file.data, metadata);
+  
+  // Listen for state changes, errors, and completion of the upload.
+  uploadTask.on('state_changed',
+    (snapshot) => {
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log('Upload is ' + progress + '% done');
+      switch (snapshot.state) {
+        case 'paused':
+          console.log('Upload is paused');
+          break;
+        case 'running':
+          console.log('Upload is running');
+          break;
+      }
+    }, 
+    (error) => {
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      switch (error.code) {
+        case 'storage/unauthorized':
+          console.log("not authorized")
+          
+          break;
+        case 'storage/canceled':
+          console.log("cancelled")
+          break;
+  
+        // ...
+  
+        case 'storage/unknown':
+        console.log("unknown error")  
+        // Unknown error occurred, inspect error.serverResponse
+          break;
+      }
+    }, 
+    () => {
+      // Upload completed successfully, now we can get the download URL
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        res.status(200).send({url: downloadURL})
+      });
+    }
+  );
+});
+
 app.post('/cards', function(req, res){
 	console.log("saving cards into db");
 	//async required because of database query
